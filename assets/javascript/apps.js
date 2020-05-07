8cd796dd51e7d5e8baf9eb1922d5e8cfbdd6e30a
Adam Wolff <adamkwolff@gmail.com>
2020-05-07 05:52:40 -0500

    buttons are working better

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,10 +1,24 @@
 $(document).ready(function () {
     $(".display-4").text("Computer Trivia");
     $("#start").append("<h3>Start</h3>");
-    $("#optionOne").append("<h3>Start</h3>");
-    $("#optionTwo").append("<h3>Start</h3>");
-    $("#optionThree").append("<h3>Start</h3>");
-    $("#optionFour").append("<h3>Start</h3>");
+
+    $("#start").on("click", function () {
+
+        $("#start").html("");
+
+        $("#optionOne").append("<h3>Q1</h3>");
+        $("#optionTwo").append("<h3>Q2</h3>");
+        $("#optionThree").append("<h3>Q3</h3>");
+        $("#optionFour").append("<h3>Q4</h3>");
+
+
+
+    });
+
+
+
+
+
 
 
 
