d88bfa415685066ee5c0546cec35a6b634eac52c
Adam Wolff <adamkwolff@gmail.com>
2020-05-07 05:43:17 -0500

    got my buttons working sort of

---
+++ assets/javascript/apps.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $(".display-4").text("Computer Trivia");
+    $("#start").append("<h3>Start</h3>");
+    $("#optionOne").append("<h3>Start</h3>");
+    $("#optionTwo").append("<h3>Start</h3>");
+    $("#optionThree").append("<h3>Start</h3>");
+    $("#optionFour").append("<h3>Start</h3>");
+
+
+
+
+
+
+});
