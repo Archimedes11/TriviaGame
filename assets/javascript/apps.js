410231818ab31d37cdfba9d954b404db6036e4b2
Adam Wolff <adamkwolff@gmail.com>
2020-05-08 07:06:15 -0500

    pausing to go study timers

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -2,11 +2,12 @@ $(document).ready(function () {
     var flagOne = false;
     rightAnswers = 0;
     wrongAnswers = 0;
-    answers = rightAnswers + wrongAnswers;
+    answers = 0;
     $(".display-4").text("Computer Trivia");
     $("#start").append("<h3>Start</h3>");
 
     var timer = 30;
+    var timerTwo = 5;
 
     staticQuestionArray = [
         {
@@ -37,6 +38,7 @@ $(document).ready(function () {
 
         }
     ]
+
     var timerId = "#timer";
     function startTimer() {
         clearInterval(timerId);
@@ -54,6 +56,21 @@ $(document).ready(function () {
         }
     }
 
+    var timerIdTwo = "";
+    function startTimerTwo() {
+        clearInterval(timerIdTwo);
+        timerIdTwo = setInterval(decrement, 1000);
+        function decrement() {
+            timerTwo--;
+            function stop() {
+                clearInterval(timerIdTwo);
+            }
+            if (timerTwo === 0) {
+                stop();
+            }
+        }
+    }
+
     function clearQuestions() {
         $("#question").html("");
         $("#optionOne").html("");
@@ -62,79 +79,87 @@ $(document).ready(function () {
         $("#optionFour").html("");
     }
 
+    function postQuestions() {
+        $("#question").html(staticQuestionArray[answers].question)
+        $("#optionOne").html(staticQuestionArray[answers].choices[0]);
+        $("#optionTwo").html(staticQuestionArray[answers].choices[1]);
+        $("#optionThree").html(staticQuestionArray[answers].choices[2]);
+        $("#optionFour").html(staticQuestionArray[answers].choices[3]);
+    }
 
-    $("#start").on("click", function () {
 
+    function gameOne() {
+        postQuestions();
+        startTimer();
+        $("#game").on("click", ".buttons", function () {
+            if (event.toElement.id === "optionThree") {
+                flagOne = true;
+                clearQuestions()
+                $("#condition").html("<h3>" + "Correct!" + "</h3>");
+                $("#image").append("<img src=" + staticQuestionArray[0].imgSource + "></img>");
+                rightAnswers++;
+                answers = rightAnswers + wrongAnswers;
+
+            }
+
+            if (event.toElement.id != "optionThree") {
+                flagOne = true;
+                clearQuestions()
+                $("#condition").html("<h3>" + "Incorrect!" + "</h3>");
+                $("#correctAnswer").html("The corect answer was : " + staticQuestionArray[0].choices[2]);
+                $("#image").append("<img src=" + staticQuestionArray[0].imgSource + "></img>");
+                wrongAnswers++;
+                answers = rightAnswers + wrongAnswers;
+
+            }
+        });
+    }
+
+
+    function gameTwo() {
+        postQuestions();
+        startTimer();
+
+        $("#game").on("click", ".buttons", function () {
+            if (event.toElement.id === "optionOne") {
+                flagOne = true;
+                clearQuestions()
+                $("#condition").html("<h3>" + "Correct!" + "</h3>");
+                $("#image").append("<img src=" + staticQuestionArray[1].imgSource + "></img>");
+                rightAnswers++;
+                answers = rightAnswers + wrongAnswers;
+
+            }
+            if (event.toElement.id != "optionOne") {
+                flagOne = true;
+                clearQuestions()
+                $("#condition").html("<h3>" + "Incorrect!" + "</h3>");
+                $("#correctAnswer").html("The corect answer was : " + staticQuestionArray[1].choices[0]);
+                $("#image").append("<img src=" + staticQuestionArray[1].imgSource + "></img>");
+                wrongAnswers++;
+                answers = rightAnswers + wrongAnswers;
+
+            }
+            return answers;
+        });
+
+    }
+
+    $("#start").one("click", function () {
+
+        $("#start").html("");
         if (answers === 0) {
-            $("#start").html("");
-            $("#question").html(staticQuestionArray[0].question)
-            $("#optionOne").html(staticQuestionArray[0].choices[0]);
-            $("#optionTwo").html(staticQuestionArray[0].choices[1]);
-            $("#optionThree").html(staticQuestionArray[0].choices[2]);
-            $("#optionFour").html(staticQuestionArray[0].choices[3]);
-
-            startTimer();
-
-            $(".buttons").on("click", function () {
-                if (event.toElement.id === "optionThree") {
-                    flagOne = true;
-                    clearQuestions()
-                    $("#condition").html("<h3>" + "Correct!" + "</h3>");
-                    $("#image").append("<img src=" + staticQuestionArray[0].imgSource + "></img>");
-                    rightAnswers++;
-                }
-
-                if (event.toElement.id != "optionThree") {
-                    flagOne = true;
-                    clearQuestions()
-                    $("#condition").html("<h3>" + "Incorrect!" + "</h3>");
-                    $("#correctAnswer").html("The corect answer was : " + staticQuestionArray[0].choices[2]);
-                    $("#image").append("<img src=" + staticQuestionArray[0].imgSource + "></img>");
-                    wrongAnswers++;
-                }
-                
-            })
-            
+            gameOne();
+            console.log("adam");
+            console.log(answers);
+            if (answers === 1) {
+                gameTwo();
+                console.log("adam");
+            }
         }
-        
-
-
-        //////////////////part 2
-
-        if (answers === 1) {
-            
-
-            $("#question").html(staticQuestionArray[1].question)
-            $("#optionOne").html(staticQuestionArray[1].choices[0]);
-            $("#optionTwo").html(staticQuestionArray[1].choices[1]);
-            $("#optionThree").html(staticQuestionArray[1].choices[2]);
-            $("#optionFour").html(staticQuestionArray[1].choices[3]);
-
-            startTimer();
-
-            $(".buttons").on("click", function () {
-                if (event.toElement.id === "optionOne") {
-                    flagOne = true;
-                    clearQuestions()
-                    $("#condition").html("<h3>" + "Correct!" + "</h3>");
-                    $("#image").append("<img src=" + staticQuestionArray[1].imgSource + "></img>");
-                }
-                if (event.toElement.id != "optionOne") {
-                    flagOne = true;
-                    clearQuestions()
-                    $("#condition").html("<h3>" + "Incorrect!" + "</h3>");
-                    $("#correctAnswer").html("The corect answer was : " + staticQuestionArray[1].choices[0]);
-                    $("#image").append("<img src=" + staticQuestionArray[1].imgSource + "></img>");
-                }
-
-            })
-        }
-
-
-
-
-
 
     });
+
+
 
 });
